6b8d9fc975c4fbd0230740c2a7861dacab8d5289
GitHub Action <action@github.com>
2021-09-29 08:49:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vasilevitzt/examplepluginphpstorm/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vasilevitzt.examplepluginphpstorm.services
+
+import com.github.vasilevitzt.examplepluginphpstorm.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/vasilevitzt/examplepluginphpstorm/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vasilevitzt/examplepluginphpstorm/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vasilevitzt.examplepluginphpstorm.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vasilevitzt.examplepluginphpstorm.MyBundle
 
 class MyProjectService(project: Project) {
 
